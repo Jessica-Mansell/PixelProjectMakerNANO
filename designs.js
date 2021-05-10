c414c31a96df119837f45d5cc42524eeaec47929
Jessica Mansell <jcmansell88@gmail.com>
2021-05-09 21:36:11 -0700

    Update designs.js
    enhanced the clicking capabilities so that the user can "paint" with pixels. Added mousedown and mouseup

--- designs.js
+++ designs.js
@@ -1,3 +1,5 @@
+var mouse_is_pressed = false;
+
 function makeGrid() {
   let width = document.getElementById('inputWidth').value;
   let height = document.getElementById('inputHeight').value;
@@ -5,13 +7,21 @@ function makeGrid() {
   let colorPicker = document.getElementById('colorPicker');
   // This clears the canvas.
   canvas.innerHTML = '';
-  // Makes the grid.
+  // Makes the grid
   for (let i = 0; i < height; i++) {
     let row = canvas.insertRow(i);
     for (let j = 0; j < width; j++) {
       let cell = row.insertCell(j);
-      cell.addEventListener('click', function (event) {
-        cell.style.backgroundColor = colorPicker.value;
+      cell.addEventListener('mouseover', function (event) {
+        if (mouse_is_pressed) {
+          cell.style.backgroundColor = colorPicker.value;
+        }
+      });
+      cell.addEventListener('mousedown', function (event) {
+        mouse_is_pressed = true;
+      });
+      cell.addEventListener('mouseup', function (event) {
+        mouse_is_pressed = false;
       });
     }
   }
